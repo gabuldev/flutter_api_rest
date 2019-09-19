fc5e9fd72e2ba6fa20f8c8bbb1b564a56b458d00
Henrique Arthur <hnrq.art@gmail.com>
2019-09-18 22:17:08 -0300

    Ajuste para createDio() ser chamado apenas uma vez

--- lib/src/shared/custom_dio/custom_dio.dart
+++ lib/src/shared/custom_dio/custom_dio.dart
@@ -3,7 +3,7 @@ import 'package:tratar_erros_dio/src/shared/constants.dart';
 import 'package:tratar_erros_dio/src/shared/custom_dio/interceptors.dart';
 
 class CustomDio {
-  createDio() {
+  Dio createDio() {
     Dio dio = Dio();
 
     dio.options.baseUrl = BASE_URL;
